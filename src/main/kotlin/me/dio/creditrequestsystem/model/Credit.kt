fdbdb397a04d3242aceb235dd355449091b3db09
Nathalia-Soares <nathalia.oliveira24@fatec.sp.gov.br>
2024-02-01 22:32:26 -0300

    Criação de Models e Interfaces de Repositories

---
+++ src/main/kotlin/me/dio/creditrequestsystem/model/Credit.kt
@@ -0,0 +1,26 @@
+package me.dio.creditrequestsystem.model
+
+import jakarta.persistence.*
+import me.dio.creditrequestsystem.enum.Status
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.UUID
+
+@Entity
+// @Table(name = "Credito")
+data class Credit(
+    @Column(nullable = false, unique = true)
+    val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false)
+    val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false)
+    val dayFirstInstallment: LocalDate,
+    @Column(nullable = false)
+    val numberOfInstallment: Int = 0,
+    @Column(nullable = false)
+    val status: Status = Status.IN_PROGRESS,
+    @ManyToOne
+    val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+    )
